6641dbca516201f1f0b40b299ac0d3b513cc8055
MichaelBuen <me@ienablemuch.com>
2015-08-26 21:21:45 +0800

    Added unit test

--- tests/just-test.ts
+++ tests/just-test.ts
@@ -5,6 +5,11 @@
 ///<reference path="../public/app-dir/Welcome/Controller.ts"/>
 ///<reference path="../typings/requirejs/require.d.ts"/>
 
+///<reference path="../public/app-dir/Product/Controller.ts"/>
+
+
+
+
 
 
 describe("Hello world",() => {
@@ -27,10 +32,12 @@ window["define"] = function(depArray : string[], c: any) {
 def(require => {
 
 
-    require("/base/shared/Domain/Person.js");
-    require("/base/shared/ViewValue/Header.js");
+
 
     describe("Person Domain", () => {
+
+        require("/base/shared/Domain/Person.js");
+        require("/base/shared/ViewValue/Header.js");
 
 
         it("says negative", () => {
@@ -40,9 +47,11 @@ def(require => {
 
     });
 
-    require('/base/public/app-dir/Welcome/Controller.js');
+
 
     describe("Welcome Controller", () => {
+
+        require('/base/public/app-dir/Welcome/Controller.js');
 
         it("says welcome", () => {
             var h = new ViewValue.Header();
@@ -50,6 +59,26 @@ def(require => {
             var ctrl = new App.Welcome.Controller(h);
             expect(ctrl.sampleMessage).toEqual("Welcome Message");
             expect(h.title).toEqual("Welcome");
+        });
+
+    });
+
+
+
+    describe("Product Controller", () => {
+
+        require('/base/public/app-dir/Product/Controller.js');
+        require('/base/shared/Domain/Product.js');
+        require('/base/shared/ViewValue/Header.js');
+
+        it("says product", () => {
+
+            var p = new Domain.Product();
+            var h = new ViewValue.Header();
+
+            var ctrl = new App.Product.Controller(p, h);
+            expect(ctrl.sampleMessage).toEqual("Product's sample message");
+            expect(h.title).toEqual("Product");
         });
 
     });
