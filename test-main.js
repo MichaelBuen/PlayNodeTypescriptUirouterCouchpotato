1ddbea9557693cd15ef80d01a4d902726348f01b
MichaelBuen <me@ienablemuch.com>
2015-08-27 10:58:32 +0800

    Guarantee modification of define function be runned first

--- test-main.js
+++ test-main.js
@@ -2,6 +2,8 @@ var allTestFiles = [];
 
 var TEST_REGEXP = /(spec|test)\.js$/i;
 
+allTestFiles.push('tests/_must-be-runned-first');
+
 // Get a list of all the test files to include
 Object.keys(window.__karma__.files).forEach(function(file) {
   if (TEST_REGEXP.test(file)) {
@@ -9,9 +11,15 @@ Object.keys(window.__karma__.files).forEach(function(file) {
     // If you require sub-dependencies of test files to be loaded as-is (requiring file extension)
     // then do not normalize the paths
     var normalizedTestModule = file.replace(/^\/base\/|\.js$/g, '');
+
     allTestFiles.push(normalizedTestModule);
   }
 });
+
+
+
+console.log(allTestFiles);
+
 
 require.config({
   // Karma serves files under /base, which is the basePath from your config file
